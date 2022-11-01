93b34c1a484e30ce2af33da33566acc7dd34fe9d
Billy Glenn <snowake4me@gmail.com>
2022-11-01 11:48:25 -0700

    broken code

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,6 @@ streamlit.dataframe(my_data_rows)
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thank you for adding ', add_my_fruit)
+
+# NonFunctional, but 'going with it'
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
